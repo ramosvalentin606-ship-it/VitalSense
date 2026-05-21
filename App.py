b3c186e1e031c88270b86592c574ca8e32557648
ramosvalentin606-ship-it <ramosvalentin606@gmail.com>
2026-05-20 21:46:04 -0500

    Update App.py

--- App.py
+++ App.py
@@ -63,7 +63,7 @@ def obtener_orientacion_medica(sintomas_texto):
     "{sintomas_texto}"
     """
     
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={API_KEY}"
     headers = {'Content-Type': 'application/json'}
     data = {
         "contents": [{"parts": [{"text": prompt_completo}]}]
